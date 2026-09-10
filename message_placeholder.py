7797224bdbc996638e7d0e6dae44d3781f3ca3a4
urjanandy23 <pusainandy37@gmail.com>
2026-09-11 02:07:58 +0530

    Enhance code readability with comments
    Added comments to clarify the purpose of each section of the code, including chat history loading and prompt creation.

--- message_placeholder.py
+++ message_placeholder.py
@@ -8,12 +8,21 @@ chat_template = ChatPromptTemplate([
 
 chat_history = []
 # load chat history
+# This opens chat_history.txt and reads its contents
+# readlines() → reads all lines from the file.
+# extend() → adds those lines to chat_history.
+
 with open('chat_history.txt') as f:
     chat_history.extend(f.readlines())
 
 print(chat_history)
 
 # create prompt
+# SYSTEM: You are a helpful customer support agent
+# CHAT HISTORY: [previous conversation]
+# HUMAN: Where is my refund?
+
 prompt = chat_template.invoke({'chat_history':chat_history, 'query':'Where is my refund'})
+
 
 print(prompt)
