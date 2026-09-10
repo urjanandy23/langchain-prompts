06b44bf8e02600c9231dcdd9faba167bfb0e0393
urjanandy23 <pusainandy37@gmail.com>
2026-09-11 01:35:14 +0530

    Enhance chatbot.py with explanatory comments
    Added comments to clarify the purpose of each code section.

--- chatbot.py
+++ chatbot.py
@@ -12,11 +12,17 @@ chat_history = [
 
 while True:
     user_input = input('You: ')
-    chat_history.append(HumanMessage(content=user_input))
+    chat_history.append(HumanMessage(content=user_input)) # Create a HumanMessage containing whatever the user typed, and add that message to the chat_history list.
+                                                          # user_input is what the user typed → HumanMessage labels it as a user message → append() stores it in chat_history.
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(AIMessage(content=result.content))
-    print("AI: ",result.content)
+    chat_history.append(AIMessage(content=result.content)) 
+# Take the AI's answer and save it in the chat_history list.
+# result.content → the AI's actual answer
+# AIMessage(...) → marks that answer as coming from the AI
+# .append(...) → adds it to chat_history
+    
+print("AI: ",result.content)
 
 print(chat_history)
